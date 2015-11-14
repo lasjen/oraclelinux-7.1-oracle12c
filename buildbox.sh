93a33ae11e4ec959d12f105bf6ba629fa98f74bc
Lasse Jenssen <lasse@jcon.no>
2015-11-14 10:57:00 +0100

    Div changes

--- buildbox.sh
+++ buildbox.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-wget -c --load-cookies=oracle_otn_cookie.txt --http-user=lasse.jenssen@evry.com --ask-password -O database_installer/source/linuxamd64_12102_database_1of2.zip "https://edelivery.oracle.com/akam/otn/linux/oracle12c/121020/linuxamd64_12102_database_1of2.zip" 
-wget -c --load-cookies=oracle_otn_cookie.txt --http-user=lasse.jenssen@evry.com --ask-password -O database_installer/source/linuxamd64_12102_database_2of2.zip "https://edelivery.oracle.com/akam/otn/linux/oracle12c/121020/linuxamd64_12102_database_2of2.zip" 
+#wget -c --load-cookies=oracle_otn_cookie.txt --http-user=lasse.jenssen@evry.com --ask-password -O database_installer/source/linuxamd64_12102_database_1of2.zip "https://edelivery.oracle.com/akam/otn/linux/oracle12c/121020/linuxamd64_12102_database_1of2.zip" 
+#wget -c --load-cookies=oracle_otn_cookie.txt --http-user=lasse.jenssen@evry.com --ask-password -O database_installer/source/linuxamd64_12102_database_2of2.zip "https://edelivery.oracle.com/akam/otn/linux/oracle12c/121020/linuxamd64_12102_database_2of2.zip" 
+wget -c --load-cookies=oracle_otn_cookie.txt --http-user=lasse.jenssen@evry.com --ask-password -O database_installer/source/p21359755_121020_Linux-x86-64.zip "https://updates.oracle.com/Orion/Services/download/p21359755_121020_Linux-x86-64.zip?aru=19194568&patch_file=p21359755_121020_Linux-x86-64.zip"
 #packer build -only=virtualbox-iso ol-7.0-x86_64.json
 
