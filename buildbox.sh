1cb1ca9fa63e36d483fb5383e0206fa6cb0208a2
Lasse Jenssen <lasse@jcon.no>
2015-11-14 02:26:16 +0100

    Second commit w/smal changes

--- buildbox.sh
+++ buildbox.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-packer build -only=virtualbox-iso ol-7.0-x86_64.json
+wget -c --load-cookies=oracle_otn_cookie.txt --http-user=lasse.jenssen@evry.com --ask-password -O database_installer/source/linuxamd64_12102_database_1of2.zip "https://edelivery.oracle.com/akam/otn/linux/oracle12c/121020/linuxamd64_12102_database_1of2.zip" 
+wget -c --load-cookies=oracle_otn_cookie.txt --http-user=lasse.jenssen@evry.com --ask-password -O database_installer/source/linuxamd64_12102_database_2of2.zip "https://edelivery.oracle.com/akam/otn/linux/oracle12c/121020/linuxamd64_12102_database_2of2.zip" 
+#packer build -only=virtualbox-iso ol-7.0-x86_64.json
 
